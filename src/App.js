030ff8ffb1ea471ed8f542bc7bc57d9240cee32f
corleyb2 <corleyb2@gmail.com>
2020-04-29 11:38:00 -0400

    make action creators and convert action type strings to string constants, refactor If to Switch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React, { useReducer } from "react";
 import "./App.css";
 
+//const bulletproof actions w string literals
+const INCREMENT_COUNTER = "INCREMENT_COUNTER";
+const DECREMENT_COUNTER = "DECREMENT_COUNTER";
+
 function reducer(state, action) {
-  if (action.type === "INCREMENT_COUNTER") {
-    return {
-      ...state,
-      counter: state.counter + 1,
-    };
-  }
-  if (action.type === "DECREMENT_COUNTER") {
-    return {
-      ...state,
-      counter: state.counter - 1,
-    };
-  } else {
-    return state;
+  switch (action.type) {
+    case INCREMENT_COUNTER:
+      return { ...state, counter: state.counter + 1 };
+    case DECREMENT_COUNTER:
+      return { ...state, counter: state.counter - 1 };
+    default:
+      return state;
   }
 }
 
 const initialState = {
   counter: 0,
 };
+
+//action creators
+function incrementCounter() {
+  return { type: INCREMENT_COUNTER };
+}
+function decrementCounter() {
+  return { type: DECREMENT_COUNTER };
+}
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -29,12 +35,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div>{state.counter}</div>
-        <button onClick={() => dispatch({ type: "INCREMENT_COUNTER" })}>
-          Increment
-        </button>
-        <button onClick={() => dispatch({ type: "DECREMENT_COUNTER" })}>
-          Decrement
-        </button>
+        <button onClick={() => dispatch(incrementCounter())}>Increment</button>
+        <button onClick={() => dispatch(decrementCounter())}>Decrement</button>
       </header>
     </div>
   );
