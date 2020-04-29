eea541c76699784e555450a89c081e855d71c846
corleyb2 <corleyb2@gmail.com>
2020-04-29 13:25:35 -0400

    update state with n, add functionality to increment/decrement by n

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useReducer } from "react";
 import "./App.css";
 
-//const bulletproof actions w string literals
 const INCREMENT_COUNTER = "INCREMENT_COUNTER";
 const DECREMENT_COUNTER = "DECREMENT_COUNTER";
+const UPDATE_N = "UPDATE_N";
+const INCREMENT_BY_N = "INCREMENT_BY_N";
+const DECREMENT_BY_N = "DECREMENT_BY_N";
 
 function reducer(state, action) {
   switch (action.type) {
@@ -11,6 +13,12 @@ function reducer(state, action) {
       return { ...state, counter: state.counter + 1 };
     case DECREMENT_COUNTER:
       return { ...state, counter: state.counter - 1 };
+    case UPDATE_N:
+      return { ...state, n: action.n };
+    case INCREMENT_BY_N:
+      return { ...state, counter: state.counter + state.n };
+    case DECREMENT_BY_N:
+      return { ...state, counter: state.counter - state.n };
     default:
       return state;
   }
@@ -18,14 +26,23 @@ function reducer(state, action) {
 
 const initialState = {
   counter: 0,
+  n: 0,
 };
 
-//action creators
 function incrementCounter() {
   return { type: INCREMENT_COUNTER };
 }
 function decrementCounter() {
   return { type: DECREMENT_COUNTER };
+}
+function updateN(n) {
+  return { type: UPDATE_N, n: n };
+}
+function incrementByN() {
+  return { type: INCREMENT_BY_N };
+}
+function decrementByN() {
+  return { type: DECREMENT_BY_N };
 }
 
 function App() {
@@ -37,6 +54,19 @@ function App() {
         <div>{state.counter}</div>
         <button onClick={() => dispatch(incrementCounter())}>Increment</button>
         <button onClick={() => dispatch(decrementCounter())}>Decrement</button>
+        <div>
+          <input
+            type="text"
+            onChange={(e) => dispatch(updateN(Number(e.target.value)))}
+          ></input>{" "}
+          <br />
+          <button onClick={() => dispatch(incrementByN())}>
+            Increment by N
+          </button>
+          <button onClick={() => dispatch(decrementByN())}>
+            Decrement by N
+          </button>
+        </div>
       </header>
     </div>
   );
